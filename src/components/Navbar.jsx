d7398122d15abc0cdbf61e8f812ed46e00662865
rami-dr18 <rami.drira12@gmail.com>
2025-11-25 23:10:29 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import dayjs from "dayjs";
+import { navIcons, navLinks } from "#constants";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/game.png" className="h-5 w-5" alt="Logo" />
+        <p className="font-bold">Rami's Portfolio</p>
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon" alt={`Icon ${id}`} />
+            </li>
+          ))}
+        </ul>
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
